80ca1f7eeb1071bf66ba8a52bf2e4280f1ff3112
Renzo1503 <renzo.1503@gmail.com>
2016-06-15 21:25:33 -0500

    Edit, Delete

--- WebDeveloper.DataAccess/ClientData.cs
+++ WebDeveloper.DataAccess/ClientData.cs
@@ -5,24 +5,9 @@ using WebDeveloper.Model;
 
 namespace WebDeveloper.DataAccess
 {
-    public class ClientData
+    public class ClientData:BaseDataAccess<Client>
     {
-        public List<Client> GetFakeData()
-        {
-            return new List<Client>
-            {
-                new Client { ID = 1, Name="Juan", LastName="Perez"},
-                new Client { ID = 2, Name="Raul", LastName="Ruidia"}
-            };
-        }
+     
 
-        public List<Client> GetList()
-        {
-            using (var dbContext = new WebContextDb())
-            {
-                return dbContext.Clients.ToList();
-            }
-        }
-
-    }
+    }    
 }
--- WebDeveloper/Controllers/ClientController.cs
+++ WebDeveloper/Controllers/ClientController.cs
@@ -4,16 +4,69 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebDeveloper.DataAccess;
+using WebDeveloper.Model;
 
 namespace WebDeveloper.Controllers
 {
     public class ClientController : Controller
     {
+        private ClientData _client = new ClientData();
         // GET: Client
         public ActionResult Index()
         {
             var client = new ClientData();
             return View(client.GetList());
         }
+
+        //Crear nuevo cliente
+        public ActionResult Create()
+        {
+            return View(new Client());
+        }
+
+        [HttpPost]  //Si no se pone esto habra dos metodos create
+        public ActionResult Create(Client client)
+        {
+            if (ModelState.IsValid)
+            {
+                _client.Add(client);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        //Modificar cliente
+        public ActionResult Edit(int id)
+        {
+            Client client = _client.GetList().Find(x => x.ID == id);
+            return View(client);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Client client)
+        {
+            if (ModelState.IsValid)
+            {                
+                _client.Update(client);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
+        //Eliminar cliente
+        public ActionResult Delete(int id)
+        {
+            Client client = _client.GetList().Find(x => x.ID == id);
+            return View(client);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Client client)
+        {            
+            _client.Delete(client);
+            return RedirectToAction("Index");            
+        }
+
     }
 }

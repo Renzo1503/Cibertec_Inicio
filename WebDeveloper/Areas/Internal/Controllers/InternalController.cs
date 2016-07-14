502264323749fbba53ac7ffa534e9898e5c28913
Renzo1503 <renzo.1503@gmail.com>
2016-07-13 22:30:16 -0500

    Areas_20160713

---
+++ WebDeveloper/Areas/Internal/Controllers/InternalController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebDeveloper.Controllers;
+
+namespace WebDeveloper.Areas.Internal.Controllers
+{
+    public class InternalController : BaseAccountController
+    {
+        // GET: Internal/Internal
+        public ActionResult Index()
+        {
+            return View(UserManager.Users.ToList());
+        }
+
+
+
+    }
+}
